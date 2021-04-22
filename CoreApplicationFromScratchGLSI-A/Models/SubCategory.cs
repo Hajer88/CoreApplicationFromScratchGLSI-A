b81fe2ad35cfaa20574bc805dfe3762a3eaeed70
TAKTAK Hajer <taktakhajer@gmail.com>
2021-04-22 10:57:28 +0200

    AddSubCategory

---
+++ CoreApplicationFromScratchGLSI-A/Models/SubCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApplicationFromScratchGLSI_A.Models
+{
+    public class SubCategory
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int categoryId { get; set; }
+        public Category category { get; set; }
+    }
+}
